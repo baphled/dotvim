d80eae031df66616fac7d53af5a09848f2400f15
Yomi Colledge <baphled@boodah.net>
2012-02-07 21:44:40 +0000

    Braid: Remove mirror 'bundle/gem-ctags'

--- bundle/gem-ctags/lib/rubygems_plugin.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems/command_manager'
-
-Gem::CommandManager.instance.register_command :ctags
-
-Gem.post_install do |installer|
-  require 'rubygems/commands/ctags_command'
-  Gem::Commands::CtagsCommand.index(installer.spec)
-end
